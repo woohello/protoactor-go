0a1dcf7808b26de6e6a5817ba5186f1d704844c7
woohello <woohello@qq.com>
2020-03-25 17:28:09 +0800

    init-v1 add comment

--- main.go
+++ main.go
@@ -1,21 +1,26 @@
 package main
 
-import "fmt"
-import "bufio"
-import "os"
+import (
+	"fmt"
+	"bufio"
+	"os"
+)
 
 func main() {
 	myActor := ActorOf(new(MyActor))
 	myActor.Tell(Hello{Name: "Roger"})
 	myActor.Tell(Hello{Name: "Go"})
+	myActor.Tell(100)
 	bufio.NewReader(os.Stdin).ReadString('\n')
 }
 
+// ActorRef 框架的使用接口
 type ActorRef interface {
 	Tell(message interface{})
 	SendSystemMessage(message interface{})
 }
 
+// ChannelActorRef 用通道Channel来实现消息的投递
 type ChannelActorRef struct {
 	actorCell *ActorCell
 }
@@ -28,29 +33,34 @@ func (ref *ChannelActorRef) SendSystemMessage(message interface{}) {
 	ref.actorCell.userMailbox <- message
 }
 
+// Actor 真正接收消息的处理部分
 type Actor interface {
 	Receive(message interface{})
 }
 
+// ActorOf 有两个功能
+// 1. 调度的初始化，以及ActorRef的构建
+// 2. 调度部分，消息的接收与分发传递处理
 func ActorOf(actor Actor) ActorRef {
 	userMailbox := make(chan interface{}, 100)
 	systemMailbox := make(chan interface{}, 100)
 	cell := &ActorCell{
-		userMailbox:   userMailbox,
-		systemMailbox: systemMailbox,
-		actor:         actor,
+		userMailbox:	userMailbox,
+		systemMailbox:	systemMailbox,
+		actor:		actor,
 	}
 	ref := ChannelActorRef{
 		actorCell: cell,
 	}
+
 	go func() {
 		for {
 			select {
 			case sysMsg := <-systemMailbox:
-                //prioritize system messages
+				// prioritize sys messages
 				cell.invokeSystemMessage(sysMsg)
 			default:
-				//if no system message is present, try read user message
+				// if no system message is present, try read user message
 				select {
 				case userMsg := <-userMailbox:
 					cell.invokeUserMessage(userMsg)
@@ -58,15 +68,16 @@ func ActorOf(actor Actor) ActorRef {
 				}
 			}
 		}
-	}()
+	} ()
 
 	return &ref
 }
 
+// ActorCell 一个中间部件，连接调度部分与Actor部分
 type ActorCell struct {
-	userMailbox   chan interface{}
-	systemMailbox chan interface{}
-	actor         Actor
+	userMailbox	chan interface{}
+	systemMailbox	chan interface{}
+	actor		Actor
 }
 
 func (cell *ActorCell) invokeSystemMessage(message interface{}) {
@@ -77,15 +88,19 @@ func (cell *ActorCell) invokeUserMessage(message interface{}) {
 	cell.actor.Receive(message)
 }
 
+// ----- Demo -----
+
 type MyActor struct{ messageCount int }
 type Hello struct{ Name string }
 
 func (state *MyActor) Receive(message interface{}) {
 	switch msg := message.(type) {
 	default:
-		fmt.Printf("unexpected type %T\n", msg) // %T prints whatever type t has
+		fmt.Printf("unexpected type %T\n", msg) // %T prints whatever type t has //whl?
 	case Hello:
-		fmt.Printf("Hello %v\n", msg.Name) // t has type bool
+		fmt.Printf("Hello %v\n", msg.Name) // t has type bool //whl?
 		state.messageCount++
+	case int:
+		fmt.Printf("Come on, number %v", msg)
 	}
 }
